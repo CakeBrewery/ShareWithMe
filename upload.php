498da1ec24d0220a0b830f22857f7d5fc2454782
CakeBrewery <cakebrewery@gmail.com>
2015-10-10 23:39:00 -0700

    save uploaded files on ./upload/ folder

--- upload.php
+++ upload.php
@@ -12,7 +12,7 @@ _END;
 if($_FILES)
 {
 	$name = $_FILES['filename']['name'];
-	move_uploaded_file($_FILES['filename']['tmp_name'], $name);
+	move_uploaded_file($_FILES['filename']['tmp_name'], 'uploads/' . $name);
 	echo "Uploaded file '$name' ";
 }
 
